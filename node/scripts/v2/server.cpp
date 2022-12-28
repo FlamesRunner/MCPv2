68b28a2343900e636a7ff724178a54285885f129
FlamesRunner <akyle2001@gmail.com>
2022-12-27 21:26:14 -0500

    New server wrapper

--- node/scripts/v2/server.cpp
+++ node/scripts/v2/server.cpp
@@ -1,229 +1,294 @@
-#include "server.h"
-#define CONSOLE_BUF_SIZE 1024
-#define MAX_CONNECTIONS 100
-#define CMD_PATH "mcp_in.sock"
-#define LOG_PATH "./server/server_console.log"
-#define DEBUG_LOG_PATH "./server/server_debug.log"
-#define JAVA_PATH "/usr/bin/java"
-#define JAVA_ARGS "-jar server.jar nogui"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <pwd.h>
+#include <grp.h>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <thread>
+#include <vector>
+#include <sstream>
 
-// define server constructor
-Server::Server(int min_mem, int max_mem, std::vector <char *> additional_args)
+void kill_process(pid_t pid)
 {
-    this->max_mem = max_mem;
-    this->min_mem = min_mem;
-    this->additional_args = additional_args;
+    // Kill the process
+    kill(pid, SIGTERM);
+
+    // Wait for the process to exit
+    int status;
+    waitpid(pid, &status, 0);
 }
 
-void Server::redir_std()
-{
-    // redirect stdout to log file
-    freopen(LOG_PATH, "a", stdout);
-    // redirect stderr to debug log file
-    freopen(DEBUG_LOG_PATH, "a", stderr);
-}
+void handle_connection(int connection_fd, int stdin_pipe) {
+    // Continuously read from the socket and write to the stdin pipe
+    char buffer[1024];
+    while (true)
+    {
+        ssize_t bytes_read = read(connection_fd, buffer, sizeof(buffer));
+        if (bytes_read < 0)
+        {
+            perror("read");
+            break;
+        }
+        else if (bytes_read == 0)
+        {
+            break;
+        }
 
-void Server::stop() {
-    // stop server
-    std::cout << "Stopping server..." << std::endl;
-    for (auto &t : client_threads) {
-        if (t.joinable()) {
-            t.join();
+        ssize_t bytes_written = write(stdin_pipe, buffer, bytes_read);
+        if (bytes_written < 0)
+        {
+            perror("write");
+            break;
         }
     }
 }
 
-void handle_connection(int client_socket, int stdin_fd)
+int main(int argc, char *argv[])
 {
-    FILE *console_file = fopen(LOG_PATH, "a");
-
-    // Read from client socket
-    char buffer[CONSOLE_BUF_SIZE];
-    std::string console_prefix = "> ";
-
-    while (recv(client_socket, buffer, CONSOLE_BUF_SIZE, 0) > 0)
+    if (argc < 2)
     {
-        // Write to console
-        fprintf(console_file, "%s%s\n", console_prefix.c_str(), buffer);
-        // Write to stdin
-        write(stdin_fd, buffer, strlen(buffer));
+        std::cerr << "Usage: " << argv[0] << " CONFIG_FILE" << std::endl;
+        return 1;
     }
 
-    // Close client socket
-    close(client_socket);
-
-    // Close console file
-    fclose(console_file);
-}
-
-void socket_listen(int stdin_fd)
-{
-    // Listen on socket and pipe input to stdin_fd
-    // Open debug log file
-    int socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
-    if (socket_fd < 0)
+    // Parse the configuration file and store the options in a map
+    std::map<std::string, std::string> options;
+    std::ifstream config_file(argv[1]);
+    std::string line;
+    while (std::getline(config_file, line))
     {
-        // print error message
-        std::cout << "Error opening socket\n" << std::endl;
-        // exit program
-        exit(1);
-    }
-
-    // set socket address
-    struct sockaddr_un socket_address;
-    socket_address.sun_family = AF_UNIX;
-    strcpy(socket_address.sun_path, CMD_PATH);
-    unlink(CMD_PATH);
-    // bind socket to address
-    if (bind(socket_fd, (struct sockaddr *)&socket_address, sizeof(socket_address)) < 0)
-    {
-        // print error message
-        std::cout << "Error binding socket\n" << std::endl;
-        // exit program
-        exit(1);
-    }
-
-    if (listen(socket_fd, MAX_CONNECTIONS) < 0)
-    {
-        std::cout << "Error listening on socket\n" << std::endl;
-        exit(1);
-    }
-
-    while (true)
-    {
-        struct sockaddr_in client_address;
-        socklen_t client_address_length = sizeof(client_address);
-
-        std::cout << "Waiting for connection..." << std::endl;
-        // accept connection
-        int client_socket = accept(socket_fd, (struct sockaddr *)&client_address, &client_address_length);
-        if (client_socket < 0)
+        size_t pos = line.find('=');
+        if (pos != std::string::npos)
         {
-            // print error message
-            std::cout << "Error accepting connection" << std::endl;
-            // exit program
+            std::string key = line.substr(0, pos);
+            std::string value = line.substr(pos + 1);
+            options[key] = value;
+        }
+    }
+
+    // Get the path of the process to start, its arguments, the output file path, and the socket path from the map
+    std::string process_path = options["EXECUTABLE"];
+    std::string process_args = options["ARGS"];
+    std::string output_path = options["OUTPUT"];
+    std::string socket_path = options["SOCKET"];
+    std::string working_dir = options["WORKING_DIR"];
+
+    // Check if root
+    if (geteuid() != 0)
+    {
+        std::cerr << "Must be run as root" << std::endl;
+        return 1;
+    }
+
+    // Try to drop to the specified effective UID and GID
+    int uid = -1;
+    int gid = -1;
+    if (options.count("UID"))
+    {
+        struct passwd *pwd = getpwnam(options["UID"].c_str());
+        if (pwd)
+        {
+            uid = pwd->pw_uid;
+            gid = pwd->pw_gid;
+        }
+    }
+    if (options.count("GID"))
+    {
+        struct group *grp = getgrnam(options["GID"].c_str());
+        if (grp)
+        {
+            gid = grp->gr_gid;
+        }
+    }
+
+    // Print out all of the config options
+    std::cout << "Process path: " << process_path << std::endl;
+    std::cout << "Process args: " << process_args << std::endl;
+    std::cout << "Output path: " << output_path << std::endl;
+    std::cout << "Socket path: " << socket_path << std::endl;
+    std::cout << "UID: " << uid << std::endl;
+    std::cout << "GID: " << gid << std::endl;
+
+    if (uid == -1 || gid == -1 || setgid(gid) < 0 || setuid(uid) < 0)
+    {
+        std::cerr << "Failed to set effective UID/GID" << std::endl;
+        return 1;
+    }
+
+    // Print out the UID and GID of the process
+    std::cout << "Successfully dropped to UID " << getuid() << " and GID " << getgid() << std::endl;
+
+    // Start the process
+    int stdin_pipefd[2];
+    if (pipe(stdin_pipefd) < 0)
+    {
+        perror("pipe");
+        return 1;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        return 1;
+    }
+    else if (pid == 0)
+    {
+        // Child process
+
+        // Redirect stdin to the pipe
+        dup2(stdin_pipefd[0], STDIN_FILENO);
+        
+        // Open the output file
+        int output_fd = open(output_path.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (output_fd < 0)
+        {
+            perror("open");
             exit(1);
         }
 
-        std::cout << "Connection accepted (" << client_socket << ")\n" << std::endl;
+        // Redirect stdout to the output file
+        dup2(output_fd, STDOUT_FILENO);
 
-        // create thread to handle connection
-        std::thread t(handle_connection, client_socket, stdin_fd);
-        t.detach();
-    }
-}
+        // Redirect stderr to the output file
+        dup2(output_fd, STDERR_FILENO);
 
-void Server::start_java_srv()
-{
-    // create thread
+        // Close the output file
+        close(output_fd);
 
-    // execute java application
-    std::string command = JAVA_PATH;
-    std::string cmd_args = "";
-
-    // add memory arguments
-    cmd_args += "-Xms" + std::to_string(min_mem) + "m ";
-    cmd_args += "-Xmx" + std::to_string(max_mem) + "m ";
-    cmd_args += JAVA_ARGS;
-
-    std::vector<char *> args;
-    std::string arg;
-    for (int i = 0; i < cmd_args.length(); i++)
-    {
-        // check if character is a space
-        if (cmd_args[i] == ' ')
+        // Change the working directory 
+        if (chdir(working_dir.c_str()) < 0)
         {
-            // add argument to array
-            args.push_back(strdup(arg.c_str()));
-            // clear argument
-            arg = "";
+            perror("chdir");
+            exit(1);
         }
-        else
+
+        // Split process_args into a char* array
+        std::vector<char*> args;
+        char* token = strtok(const_cast<char*>(process_args.c_str()), " ");
+        while (token != NULL)
         {
-            // add character to argument
-            arg += cmd_args[i];
+            args.push_back(token);
+            token = strtok(NULL, " ");
+        }
+        args.push_back(NULL);
+
+        // Start the process
+        if (execve(process_path.c_str(), args.data(), environ) < 0)
+        {
+            perror("execve");
+            exit(1);
         }
     }
-
-    // process additional arguments
-    for (int i = 0; i < additional_args.size(); i++)
+    else
     {
-        // add argument to array
-        args.push_back(strdup(additional_args[i]));
+        // Parent process
+        std::cout << "Started process with PID " << pid << std::endl;
+
+        // Start listener
+        pid_t listener_pid = fork();
+
+        if (listener_pid < 0)
+        {
+            perror("fork");
+            kill_process(pid);
+            return 1;
+        } else {
+            if (listener_pid == 0) {
+                // Child process
+
+                // Open the socket
+                int socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+                if (socket_fd < 0)
+                {
+                    perror("socket");
+                    exit(1);
+                }
+
+                // Bind the socket
+                struct sockaddr_un addr;
+                memset(&addr, 0, sizeof(struct sockaddr_un));
+                addr.sun_family = AF_UNIX;
+
+                // Unlink socket_path if it already exists
+                if (access(socket_path.c_str(), F_OK) == 0)
+                {
+                    if (unlink(socket_path.c_str()) < 0)
+                    {
+                        perror("unlink");
+                        exit(1);
+                    }
+                }
+
+                strncpy(addr.sun_path, socket_path.c_str(), sizeof(addr.sun_path) - 1);
+                if (bind(socket_fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) < 0)
+                {
+                    perror("bind");
+                    exit(1);
+                }
+
+                // Listen on the socket
+                if (listen(socket_fd, 1) < 0)
+                {
+                    perror("listen");
+                    exit(1);
+                }
+
+                // Continually accept connections and handle them
+                while (true)
+                {
+                    // Accept a connection
+                    int connection_fd = accept(socket_fd, NULL, NULL);
+                    if (connection_fd < 0)
+                    {
+                        perror("accept");
+                        exit(1);
+                    }
+
+                    // Handle the connection
+                    handle_connection(connection_fd, stdin_pipefd[1]);
+                }
+
+                // Close the read end of the stdin pipe
+                close(stdin_pipefd[0]);
+
+                // Wait for the process to exit
+                int status;
+                waitpid(pid, &status, 0);
+
+                // Exit
+                exit(0);
+            } else {
+                // Parent process
+
+                std::cout << "Started listener with PID " << listener_pid << std::endl;
+
+                // Close the read end of the stdin pipe
+                close(stdin_pipefd[0]);
+
+                // Wait for the launched process to exit
+                int status;
+                waitpid(pid, &status, 0);
+
+                std::cout << "Process exited with status " << status << std::endl;
+
+                // Kill the listener
+                kill_process(listener_pid);
+
+                std::cout << "Killed listener with PID " << listener_pid << std::endl;
+
+                // Delete the socket
+                unlink(socket_path.c_str());
+
+                // Exit
+                std::cout << "Exiting" << std::endl;
+                return 0;
+            }
+        }
     }
-
-    // add null pointer to end of array
-    args.push_back(NULL);
-
-    // setup pipe to allow socket listener to write to stdin
-    int stdin_fd[2];
-    if (pipe(stdin_fd) < 0)
-    {
-        // print error message
-        std::cout << "Error creating pipe" << std::endl;
-        // exit program
-        exit(1);
-    }
-    // redirect stdin to pipe
-    dup2(stdin_fd[0], 0);
-
-    // start socket listener
-    int pid = fork();
-    if (pid == 0)
-    {
-        // close read end of pipe
-        close(stdin_fd[0]);
-        // start socket listener
-        socket_listen(stdin_fd[1]);
-    }
-    else if (pid < 0)
-    {
-        // error
-        // print error message
-        std::cout << "Error forking process" << std::endl;
-        // exit program
-        exit(1);
-    } else {
-        // close write end of pipe
-        close(stdin_fd[1]);
-    }
-
-    std::cout << "Starting server..." << std::endl;
-
-    // convert argument array to char* array
-    char **argv = new char *[args.size()];
-    std::copy(args.begin(), args.end(), argv);
-
-
-    // redirect stdout to log file
-    freopen(LOG_PATH, "w", stdout);
-    // redirect stderr to debug log file
-    freopen(LOG_PATH, "a", stderr);
-    // change working directory to server directory
-    chdir("./server");
-    // execute command
-    execv(command.c_str(), argv);
-}
-
-void Server::start() {
-    this->server_thread = std::thread(&Server::start_java_srv, this);
-}
-
-void Server::wait()
-{
-    // wait for server thread to finish then return control to main thread
-    // i.e., wait for thread to exit
-    if (this->server_thread.joinable()) {
-        this->server_thread.join();
-    }
-}
-
-Server::~Server()
-{
-    // close client socket
-    close(client_socket);
-    // close server socket
-    close(server_socket);
-    // delete socket file
-    unlink(CMD_PATH);
 }
